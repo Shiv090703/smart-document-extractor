745d40493c836a45d96c2eaf0fad9da1fa761e4a
Shiv090703 <jarvisr9879@gmail.com>
2025-10-01 11:57:50 +0530

    update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ UPLOAD_FOLDER = "invoices"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 # Static and template setup
-app.mount("/static", StaticFiles(directory="static"), name="static")
+#app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
